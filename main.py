52abb4ad10a3fd152ad485467ed2a1b5499840ff
Korngpop <korngpop0954201261@gmail.com>
2025-09-10 13:46:41 +0700

    com

---
+++ main.py
@@ -0,0 +1,7 @@
+from myfunc import add
+from myfunc2 import add
+def main():
+  a = float(input("ป้อนค่า A :"))
+  b = float(input("ป้อนค่า B :"))
+  result = add(a,b)
+  print(f"ผลลัพธ์ของ {a} + {b} คือ {result}"))
